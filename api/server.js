6df322fda1d251f9fa023c953ff7c1139d973618
honnage <hon_da-1998@hotmail.com>
2023-05-14 11:28:50 +0700

    update config server and update views

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-import morgan  from 'morgan'
-import cors  from 'cors'
+import morgan from 'morgan'
+import cors from 'cors'
 
 require('dotenv').config()
 // import router from './routes/api'
@@ -15,22 +15,25 @@ const trackerRoutes = require('./routes/web/tracker')
 // controllers
 const errorController = require('./controllers/error') // controllers error
 
-const port =  process.env.SERVER_POST || 3066
+const port = process.env.SERVER_POST || 3066
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(morgan('dev'))
+
+app.use(morgan('dev', {
+    skip: (req, res) => req.path.startsWith('/assets')
+}))
 app.use(cors())
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 // app.use('api', router)
 app.use(trackerRoutes)
 app.use(errorController.get404) // page not found
-
 
 app.listen(port, () => {
     console.log(`Server runnning on port ${port}`)
