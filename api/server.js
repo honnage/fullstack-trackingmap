9e107e05c120c6ed763bf82f7aec73f3e8f0511c
honnage <hon_da-1998@hotmail.com>
2023-05-15 03:43:53 +0700

    update query

--- api/server.js
+++ api/server.js
@@ -39,7 +39,7 @@ app.use(morgan('dev', {
 }))
 app.use(cors())
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.text())
 
--- api/services/transactions.js
+++ api/services/transactions.js
@@ -1,9 +1,36 @@
+const moment = require('moment')
 const Transactions = require('../models/transactions')
 
+const sequelize = require('../util/database')
+
 exports.lastTracing_byDevices = async (deviceId) => {
-    const devices = await Transactions.findAll(
-        { raw: true }
-    )
+    const { QueryTypes } = require('sequelize')
+    const devices = sequelize.query(
+        `SELECT trans.*,
+        DATE_FORMAT(trans.createdAt, '%Y-%m-%d %H:%i:%s') AS formattedCreatedAt
+         FROM transactions trans,
+              (SELECT transactions.deviceId, MAX(transactions.createdAt) AS lastDate
+               FROM transactions
+               GROUP BY transactions.deviceId) max_date
+         WHERE trans.deviceId = max_date.deviceId
+           AND trans.createdAt = max_date.lastDate
+         ORDER BY trans.createdAt DESC`,
+        {
+          type: QueryTypes.SELECT,
+          raw: true
+        }
+      )
+      console.log( JSON.stringify(devices)) // Promise { <pending> }
+
+    for (let i = 0; i < devices.length; i++) {
+        console.log(devices[i])
+        if (devices[i].createdAt == 'Invalid Date') {
+            devices[i].createdAt = '-'
+        } else {
+            devices[i].createdAt = moment(devices[i].createdAt).format('YYYY-MM-DD HH:mm:ss')
+        }
+        // console.log('createdAt', devices[i].createdAt)
+    }
 
     return devices
 }
