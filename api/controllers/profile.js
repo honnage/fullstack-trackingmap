5fb30d0bc4c8de0434aa00763765f35b686bb0ba
honnage <hon_da-1998@hotmail.com>
2023-05-16 00:25:41 +0700

    view profile

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -1,18 +1,26 @@
 const Users = require('../models/user')
-const bcrypt = require('bcrypt')
-const express = require('express')
 
+const devicesServices = require('../services/devices')
+const transactionsServices = require('../services/transactions')
 
 exports.getProfile = async (req, res, next) => {
-  try {
-      res.render('profile', {
-          // data: data,
-          pageTitle: 'Profile',
-          deviceId: '',
-          path: '/profile'
-      })
-  } catch (error) {
-      console.log(error);
-      next(error);
-  }
+    try {
+        const devices = await devicesServices.getAllDevices(req)
+        const transactions = await transactionsServices.lastTracing_byDevices(req)
+
+        const data = {
+            devices: devices,
+            tracing: transactions
+        }
+
+        res.render('profile', {
+            data: data,
+            pageTitle: 'Profile',
+            deviceId: '',
+            path: '/profile'
+        })
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 }
