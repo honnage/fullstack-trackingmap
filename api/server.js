aa5e74bbb770d6113de4d4457e29621ebbd79881
honnage <hon_da-1998@hotmail.com>
2023-05-16 23:31:55 +0700

    fix config server

--- api/server.js
+++ api/server.js
@@ -6,8 +6,6 @@ const cors = require('cors')
 const cookieSession = require('cookie-session')
 const moment = require('moment')
 const momenttz = require('moment-timezone');
-
-
 require('dotenv').config()
 
 // models
@@ -30,6 +28,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(cors())
+app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.text())
@@ -47,35 +46,43 @@ morgan.token('time', (req, res, tz) => {
 })
 
 morgan.token('sessionUser', (req, res, tz) => {
-    let sessionUser = ' SERVER '
+    let sessionUser = 'SERVER'
     if (req.session.user) [
         sessionUser =  req.session.user.username
     ]
     return sessionUser
 })
 
-const customFormat = ':time => :method :url :status :response-time ms - :res[content-length]  ||  CALL API BY  =>  :sessionUser ';
+const customFormat = ':time :method :url :status :response-time ms - :res[content-length] :sessionUser ';
 
 // show log and save log to database
 app.use(morgan(customFormat, {
-    // stream: {
-    //     write: function (message) {
+    stream: {
+        write: function (message) {
+            console.log(message)
+            const logData = message.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, '').split(' ');
+            // console.log('logData', logData)
+            let data = {
+                method: logData[2],
+                path: logData[3],
+                status: logData[4],
+                timeProcess: logData[5] + ' ' + logData[6] + ' ' + logData[7] + ' ' + logData[8],
+                callApiBy: logData[9],
+                timestamp: new Date()
+            }
+            // console.log('data', data)
 
-    //         // console.log(message)
-    //         // const logData = message.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, '').split(' ');
-    //         // console.log('logData', logData)
-
-    //         // Log.create({
-    //         //     method: logData[0],
-    //         //     path: logData[1],
-    //         //     status: logData[2],
-    //         //     timeProcess: logData[3] + ' ' + logData[4] + ' ' + logData[5] + ' ' + logData[6],
-    //         //     level: 'info',
-    //         //     callApiBy: '', // Assuming session.user contains the caller information
-    //         //     timestamp: new Date()
-    //         // });
-    //     }
-    // },
+            // Log.create({
+            //     method: data.method,
+            //     path: data.path,
+            //     status: data.status,
+            //     timeProcess: data.timeProcess,
+            //     level: 'info',
+            //     callApiBy: data.callApiBy, // Assuming session.user contains the caller information
+            //     timestamp: data.timestamp
+            // });
+        }
+    },
     skip: (req, res) => {
         return (
             req.path.startsWith('/assets') ||
@@ -84,31 +91,6 @@ app.use(morgan(customFormat, {
         );
     }
 }));
-
-
-// app.use(morgan('dev', {
-//     stream: {
-//         write: function (message) {
-//             console.log(message)
-//             const logData = message.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, '').split(' ')
-//             Log.create({
-//                 method: logData[0],
-//                 path: logData[1],
-//                 status: logData[2],
-//                 timeProcess: logData[3] + ' ' + logData[4] + '' + logData[5] + '' + logData[6],
-//                 level: 'info',
-//                 callApiBy: '',
-//                 timestamp: new Date()
-//             })
-//         }
-//     },
-//     skip: (req, res) => {
-//         return (
-//             req.path.startsWith('/assets') ||
-//             req.path.startsWith('/device/assets')
-//         )
-//     }
-// }))
 
 
 // app.use('api', router)
